8d039405f7aa78acb981725063bcf46b42dc6ac8
jWit5669 <71896257+jWit5669@users.noreply.github.com>
2025-05-02 15:36:22 -0400

    I made some minor changes, but I'm not exactly sure what they all are. I believe I changed the lat/long function to remove the location after making the geopoint

--- bigdataproject/clean_data.py
+++ bigdataproject/clean_data.py
@@ -10,16 +10,19 @@ def add_geopoint( collection ):
         to make a single geopoint that can be plotted on a map in Atlas
     """
 
-    for doc in collection.find( {"location" : {"$exists": True}}):
+    for document in collection.find( {"location" : {"$exists": True}}):
         try:
-            latitude = float( doc.get( "latitude" ) )
-            longitude = float( doc.get( "longitude" ) )
+            latitude = float( document.get( "latitude" ) )
+            longitude = float( document.get( "longitude" ) )
             collection.update_one(
-                {"_id" : doc["_id"]},
-                {"$set" : {"geopoint" : {"type" : "Point", "coordinates" : [longitude, latitude]}}}
+                {"_id" : document["_id"]},
+                {
+                    "$set" : {"geopoint" : {"type" : "Point", "coordinates" : [longitude, latitude]}},
+                    "$unset" : {"location": ""}
+                }
             )
         except ( TypeError, ValueError ):
-            print( f"Skipping document with _id: {doc['_id']} due to invalid lat/lon." )
+            print( f"Skipping document with _id: {document['_id']} due to invalid lat/lon." )
 
 
 def clean_datetimes( collection ):
@@ -27,20 +30,20 @@ def clean_datetimes( collection ):
         All of the date and time information is in the form of 2 fields: date and time
         These can be combined into one field with date and time in it
     """
-    for doc in collection.find( {"crash_time" : {"$exists": True}} ):
+    for document in collection.find( {"crash_time" : {"$exists": True}} ):
         try:
             
-            crash_date = doc.get( "crash_date" )
-            crash_time = doc.get( "crash_time" )
+            crash_date = document.get( "crash_date" )
+            crash_time = document.get( "crash_time" )
 
-            if isinstance( crash_date, str ) and crash_time:
+            if crash_date and crash_time:
                 
                 crash_date_date = datetime.strptime(crash_date[:10].strip(), "%Y-%m-%d").date()
                 crash_date_time = datetime.strptime( crash_time.strip(), "%H:%M" ).time()
                 combined_datetime = datetime.combine( crash_date_date, crash_date_time )
 
                 collection.update_one(
-                {"_id" : doc["_id"]},
+                {"_id" : document["_id"]},
                 {
                     "$set" : {"crash_date" : combined_datetime},
                     "$unset" : {"crash_time" : ""}
@@ -48,7 +51,7 @@ def clean_datetimes( collection ):
             )
                 
         except Exception as e:
-            print( f"Error processing document {doc['_id']}: {e}" )
+            print( f"Error processing document {document['_id']}: {e}" )
 
 
 
